5190f51da79f6e65927cf033100b75f901215e75
Erwin Janssen <erwinjanssen@outlook.com>
2019-06-11 15:59:53 +0200

    Scripts to generate dockerfiles and .drone.yml
    The Python script 'update_repositories.py' will generate dockerfiles and
    a .drone.yml build script for every implementation in this repository.
    So far these are 'botan', 'mbedtls' and 'openssl'.

---
+++ update_repositories.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path
+from importlib import import_module
+from distutils.version import LooseVersion
+
+import pygit2
+from jinja2 import Template
+
+# Every implementation is stored in a separate directory, looping over them
+# allows updated each of them.
+implementations = [
+    ("botan", "https://github.com/randombit/botan.git"),
+    ("mbedtls", "https://github.com/ARMmbed/mbedtls.git"),
+    ("openssl", "https://github.com/openssl/openssl.git"),
+]
+
+for (implementation, url) in implementations:
+    path = Path(implementation)
+    repo = pygit2.Repository(pygit2.discover_repository(implementation))
+
+    upstream_directory = str(path / "upstream")
+    upstream = pygit2.Repository(pygit2.discover_repository(upstream_directory))
+
+    # TODO: Update the upstream repository and fetch the latest tags
+
+    # Get a list of all Git tags in the upstream
+    tags = [ref for ref in upstream.references if ref.startswith("refs/tags/")]
+
+    # Remove the 'refs/tags/' prefix from the tag names
+    tags = [tag.replace("refs/tags/", "") for tag in tags]
+
+    # Filter the rest of the tags with implementation dependent rules, and
+    # extract the actual version numbers from the tags.
+    module = import_module(implementation)
+    version_info = module.extract_versions(tags)
+
+    # Read the Dockerfile template
+    with open(path / "Dockerfile.j2") as f:
+        template = Template(f.read())
+
+    for tag, version in version_info:
+        # Create the folder for this tag
+        tag_dir = path / "dockerfiles" / version
+        tag_dir.mkdir(parents=True, exist_ok=True)
+
+        # Write the created Dockerfile to the folder
+        with open(tag_dir / "Dockerfile", "w") as f:
+            f.write(template.render(implementation=implementation, url=url, tag=tag, version=version))
+
+    # Generate a .drone.yml that builds each Dockerfile
+    with open(".drone.yml.j2") as f:
+        drone_template = Template(f.read())
+
+    with open(path / ".drone.yml", "w") as f:
+        versions = sorted([version for _, version in version_info], key=LooseVersion)
+        f.write(drone_template.render(implementation=implementation, versions=versions))
